70feb8ef89216b1d9c7d7dd2d205a0762a66335f
Silas <silas898esperto@gmail.com>
2020-07-28 11:12:33 -0300

    Exercicio Angular Diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FatecDiretivas';
+  textoBotao = "Esconder/Exibir";
+  esconder = false;
+
+  cursos = [
+    { nome: "Analise de sistemas", disponibilidade: "Indisponivel"  },
+    { nome: "Eventos", disponibilidade: "Disponivel" }
+  ]
+  adicionar(nome, disponibilidade) { 
+    this.cursos = [{ nome: nome, disponibilidade: disponibilidade }, ...this.cursos]; 
+  }
+  alterarExibicao() {
+    this.esconder = !this.esconder;
+  }
 }
